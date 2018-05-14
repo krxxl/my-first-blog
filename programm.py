f7efc697e292799e08e5999215c7676918c6c64d
krxxl <krxxl@mail.ru>
2018-05-14 13:02:16 +0400

    My Django app, first commit

---
+++ programm.py
@@ -0,0 +1,8 @@
+print('Input yout Lastname?')
+lastname = input()  # считываем строку и кладём её в переменную name
+if lastname.lower() == 'pogodaev':
+    print('Pogodaev, don\'t khlupai. You fuckin faggot')
+if lastname.lower() == 'погодаев':
+    print('Погодаев, не хлюпай. Сарный ты говнюк')
+else:
+    print('You are awesome!')
